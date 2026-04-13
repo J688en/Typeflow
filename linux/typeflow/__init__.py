74d72be34d571e18f363a7066339ab3cc174726f
J688en <jphoffman28@gmail.com>
2026-04-13 14:05:53 +0000

    Deploy website to GitHub Pages

--- linux/typeflow/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-TypeFlow - Natural Human Typing Simulator for Linux
-A GTK4/libadwaita application that simulates realistic human typing.
-"""
-
-__version__ = "1.0.0"
-__app_id__ = "com.typeflow.app"
-__app_name__ = "TypeFlow"
